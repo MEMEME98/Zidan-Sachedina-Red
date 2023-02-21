26c35d595a1f71ba013372fc08afe2f6cf81c8fb
Cassidy <98993706+MEMEME98@users.noreply.github.com>
2023-02-21 18:00:25 -0500

    working on sulky slimes step 32 i think

---
+++ Sulky Slimes - ZS/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    [Header("Mouse Info")]
+    public Vector3 clickStartLocation;
+
+    [Header("Physics")]
+    public Vector3 launchVector;
+    public float launchForce;
+
+    [Header("Slime")]
+    public Transform slimeTransform;
+    public Rigidbody slimeRigidbody;
+    public Transform originalSlimePosition;
+    public GameObject slime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickStartLocation = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouseDifference = clickStartLocation - Input.mousePosition;
+            launchVector = new Vector3(mouseDifference.x * 1f, mouseDifference.y * 1.2f, mouseDifference.y * 1.5f);
+            slimeTransform.position = originalSlimePosition.position - launchVector / 400;
+            launchVector.Normalize();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            slimeRigidbody.isKinematic = false;
+            slimeRigidbody.AddForce(launchVector * launchForce, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            slime.transform.position = originalSlimePosition.position;
+            slime.transform.rotation = originalSlimePosition.rotation;
+            slimeRigidbody.isKinematic = true;
+            Debug.Log("work");
+        }
+    }
+}
