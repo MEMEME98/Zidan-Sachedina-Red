bec36aed7777a540e2e973c6f8483df80a8c2a58
#MidlifeCrisis <98993706+MEMEME98@users.noreply.github.com>
2023-03-02 18:03:06 -0500

    working on sulky slimes. my code is not good enough for the masters needs

--- Sulky Slimes - ZS/Assets/Scripts/Collectable.cs
+++ Sulky Slimes - ZS/Assets/Scripts/Collectable.cs
@@ -4,35 +4,72 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public float distanceToMove;
     public GameObject endingPos;
-    private Vector3 startingPosition;
-    private Vector3 endingPosition;
+    private Vector3 initialStartingPosition;
+    private Vector3 initialEndingPosition;
+    private Vector3 currentStart;
+    private Vector3 currentEnd;
+    private Vector3 temp;
     public float speed = 0.1f;
     public float direction = 1f;
     public Rigidbody RB;
-    
+    public float duration;
+    float timeElapsed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        startingPosition = this.transform.position;
-        endingPos.transform.position = endingPosition;
-        RB.AddForce(speed * direction, 0, 0, ForceMode.Impulse);
+        initialStartingPosition = this.transform.position;
+        initialEndingPosition = endingPos.transform.position;
+        currentStart = initialStartingPosition;
+        currentEnd = initialEndingPosition;
+        //RB.AddForce(speed * direction, 0, 0, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        RB.velocity = new Vector3(speed,0,0);
-        if(this.transform.position.x <= endingPosition.x)
+        
+
+        //while(timeElapsed < duration)
+        //{
+        //    float t = timeElapsed / duration;
+        //    transform.position = Vector3.Lerp(currentStart, currentEnd, t);
+        //    timeElapsed += Time.deltaTime;
+        //}
+
+        float t = timeElapsed / duration;
+        transform.position = Vector3.Lerp(currentStart, currentEnd, t);
+        timeElapsed += Time.deltaTime;
+
+        if (currentStart == initialStartingPosition && currentEnd == initialEndingPosition)
         {
-            direction = 1f;
-            RB.AddForce(speed * direction, 0, 0, ForceMode.Impulse);
+            temp = initialStartingPosition;
+            initialStartingPosition = initialEndingPosition;
+            initialEndingPosition = temp;
         }
-        if (this.transform.position.x >= startingPosition.x)
+        else if(currentStart == initialEndingPosition && currentEnd == initialStartingPosition)
         {
-            direction = -1f;
-            RB.AddForce(speed * direction, 0, 0, ForceMode.Impulse);
+            temp = initialEndingPosition;
+            initialEndingPosition = initialStartingPosition;
+            initialStartingPosition = temp;
         }
+        
+        if(timeElapsed >= duration)
+        {
+            timeElapsed = 0;
+        }
+
+        //RB.velocity = new Vector3(speed,0,0);
+        //if(this.transform.position.x <= endingPosition.x)
+        //{
+        //    direction = 1f;
+        //    RB.AddForce(speed * direction, 0, 0, ForceMode.Impulse);
+        //}
+        //if (this.transform.position.x >= startingPosition.x)
+        //{
+        //    direction = -1f;
+        //    RB.AddForce(speed * direction, 0, 0, ForceMode.Impulse);
+        //}
     }
 }
