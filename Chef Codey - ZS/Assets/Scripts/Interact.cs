c5209ee251a9c1c795f6aad55f5ad648b0047180
ikissmyhomiesgnanditsnotgay <106355935+LeahC0201@users.noreply.github.com>
2023-04-11 16:59:30 -0400

    working on bread picking up thing

---
+++ Chef Codey - ZS/Assets/Scripts/Interact.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public string triggerName = "";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("Space");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerName = other.name;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerName = "";
+    }
+}
